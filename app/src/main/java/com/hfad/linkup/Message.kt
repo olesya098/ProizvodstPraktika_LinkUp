557675db0b08ffc985e58d7bd1d4ae00d88fdcee
Olesya Lekareva <108861025+olesya098@users.noreply.github.com>
2025-05-10 11:15:33 +0300

    Delete app directory

--- app/src/main/java/com/hfad/linkup/Message.kt
+++
@@ -1,7 +0,0 @@
-package com.hfad.linkup
-data class Message(
-    val id: String = "", // Добавляем уникальный идентификатор сообщения
-    val text: String = "",
-    val author: String = "",
-    val timestamp: Long = System.currentTimeMillis()
-)
--- app/src/main/java/com/hfad/linkup/User.kt
+++
@@ -1,7 +0,0 @@
-package com.hfad.linkup
-data class User(
-    val id: String = "",
-    val name: String? = null,
-    val email: String? = null,
-    val avatarUrl: String? = null
-)
